1140777cab8a9826abf9615fe287732e9fdc6c56
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-03-03 17:33:32 -0300

    changing Server-side to Static Site Generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import styles from './home.module.scss';
 
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
+
+//client-side
+//server-side (exige mais processamento)
+//static site generation
+
+//ex:
+//post de um blog pode ser SSG, todos vao acessar o mesmo post e ele nao precisa se alterar
+//Comentarios do post tem que ser ClientSide pq só precisa dos comentarios depois da pagina ter carregado
+
 
 interface HomeProps {
   product: {
@@ -36,7 +45,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1MhckaFRKjK2a2idgn4H1j2E')
 
   const product = {
@@ -50,6 +59,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 horas
   }
 }
